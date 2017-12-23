8ae0e41b65639282c57cb138b63ce1190b567a59
Alan (Quey-Liang) Kao <alankao@andestech.com>
2017-12-23 16:10:22 +0800

    readelf: complete the implementation of -l and -S
    [ITHOME 鐵人賽][Day 06]

--- readelf/readelf.go
+++ readelf/readelf.go
@@ -23,6 +23,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"text/tabwriter"
 
 	"github.com/NonerKao/go-binutils/common"
@@ -70,13 +71,45 @@ func (reu *readelfUtil) Run(args map[string]interface{}) error {
 		reu.raw["h"] = raw
 	}
 
+	if *args["l"].(*bool) {
+		str := "]"
+		for _, p := range reu.file.Progs {
+			raw, err := json.Marshal(p)
+			if err != nil {
+				return err
+			}
+
+			str = "," + string(raw) + str
+		}
+		re, _ := regexp.Compile("^,")
+		str = re.ReplaceAllString(str, "[")
+
+		reu.raw["l"] = []byte(str)
+	}
+
+	if *args["S"].(*bool) {
+		str := "]"
+		for _, p := range reu.file.Sections {
+			raw, err := json.Marshal(p)
+			if err != nil {
+				return err
+			}
+
+			str = "," + string(raw) + str
+		}
+		re, _ := regexp.Compile("^,")
+		str = re.ReplaceAllString(str, "[")
+
+		reu.raw["S"] = []byte(str)
+	}
+
 	return nil
 }
 
 func (reu *readelfUtil) Output(args map[string]interface{}) error {
 
 	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	w.Init(os.Stdout, 0, 8, 1, ' ', 0)
 
 	if *args["h"].(*bool) {
 		var output elf.FileHeader
@@ -92,6 +125,36 @@ func (reu *readelfUtil) Output(args map[string]interface{}) error {
 		fmt.Fprintln(w, "\tABIVersion:\t", output.ABIVersion)
 		fmt.Fprintln(w, "\tType:\t", output.Type.GoString())
 		fmt.Fprintln(w, "\tMachine:\t", output.Machine.GoString())
+		fmt.Fprintln(w)
+		w.Flush()
+	}
+
+	if *args["l"].(*bool) {
+		var output []elf.ProgHeader
+		json.Unmarshal(reu.raw["l"], &output)
+
+		fmt.Fprintln(w, "Program Header:\t\t\t\t\t\t\t")
+		fmt.Fprintln(w, "Number\tType\tFlags\tOffset\tAddress\tFile Size\tMemory Size\tAlignment")
+		for i, p := range output {
+			fmt.Fprintf(w, "%d\t%s\t%s\t%x\t%x\t%d\t%d\t0x%x\n",
+				i, p.Type.GoString(), p.Flags.GoString(),
+				p.Off, p.Vaddr, p.Filesz, p.Memsz, p.Align)
+		}
+		fmt.Fprintln(w)
+		w.Flush()
+	}
+
+	if *args["S"].(*bool) {
+		var output []elf.SectionHeader
+		json.Unmarshal(reu.raw["S"], &output)
+
+		fmt.Fprintln(w, "Section Header:\t\t\t\t\t\t\t\t\t")
+		fmt.Fprintln(w, "Number\tName\tType\tFlags\tAddress\tOffset\tSize\tLink\tInfo\tAlignment")
+		for i, s := range output {
+			fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%x\t%x\t%d\t%d\t%d\t0x%x\n",
+				i, s.Name, s.Type.GoString(), s.Flags.GoString(),
+				s.Addr, s.Offset, s.Size, s.Link, s.Info, s.Addralign)
+		}
 		fmt.Fprintln(w)
 		w.Flush()
 	}
